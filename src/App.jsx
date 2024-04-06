9a9582f58bb5f29eb805abf7b382c0bd9f8a4a65
Joshuacodeat10 <joshuaagboola99@gmail.com>
2024-04-06 11:08:37 +0100

    Final push.
    FInal push, score counter fixed, removed automatic restarts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from 'react'
 function App() {
-  
+  //created objected grid for the tic tac toe game
   let [playerr, setPlayer] = useState({
     index1:'',index2:'',index3:'' ,
     index4:'',index5:'',index6:'',
     index7:'',index8:'',index9:''
   }
   )
+  //Useeffect for detecting when someones wins the game, did this to bypass useStates asychronous updates
   useEffect(( )=>{
+    findDraw();
     checkWin();
   }, [playerr])
+  const [hasWon, setHasWon] = useState(false)
+  //To set the text a player sees when they win
   const[wintext, setWinText]= useState('');
+  //Usestate variable For toggling between letters
+  const [toggleplayer, setTogglePlayer] = useState(true)
+  //For settin g the letter, workswith the toggling function
   const[letter, setLetter] = useState('X')
+  const[player1Score, SetPlayer1Score] = useState(0)
+  const[player2Score, SetPlayer2Score] = useState(0)
+  //toggle function
   const handleClick = () =>{
     const array=['X','O']
     setLetter(array[0])   
@@ -26,85 +36,255 @@ function App() {
     }
     console.log(toggleplayer)
   }
-  const [toggleplayer, setTogglePlayer] = useState(true)
-  const checkWin = () =>{
-    
-  if(playerr.index1 !== ''){
-    if(playerr.index1 === playerr.index1  && playerr.index2 === playerr.index1 && playerr.index3  === playerr.index1 ){
-      setWinText(playerr.index1 +  '  Wins')
+  
+  const checkWin = () =>{    
+    if(playerr.index1 !== ''){
+      if(playerr.index1 === playerr.index1  && playerr.index2 === playerr.index1 && playerr.index3  === playerr.index1 ){
+        if(hasWon === false){
+        setHasWon(true)
+       
+        setWinText(playerr.index1 +  '  Wins')
+        setLetter('')
+        
+        
+        if(playerr.index1 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index1 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+      }
+      }
+      if(playerr.index1  === playerr.index1 && playerr.index5  === playerr.index1 && playerr.index9  === playerr.index1){
+        if(hasWon === false){
+        setWinText(playerr.index1 + '  Wins')
+        setLetter('')
+       
+        setHasWon(true)
+        if(playerr.index1 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index1 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+      }
+      }
+      if(playerr.index1  === playerr.index1 && playerr.index4  === playerr.index1 && playerr.index7  === playerr.index1){
+        if(hasWon === false){
+        setHasWon(true)
+        setWinText(playerr.index1 + '  Wins')
+         setLetter('')
+        if(playerr.index1 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index1 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+      }
+      }
     }
-    if(playerr.index1  === playerr.index1 && playerr.index5  === playerr.index1 && playerr.index9  === playerr.index1){
-      setWinText(playerr.index1, 'Wins')
+    if(playerr.index2 !== ''){
+      if(playerr.index2  === playerr.index2 && playerr.index5  === playerr.index2 && playerr.index8  === playerr.index2){
+        if(hasWon === false){
+          setWinText(playerr.index2 + '  Wins')
+          setLetter('')
+          setHasWon(true)
+          if(playerr.index2 === 'X'){
+            SetPlayer1Score(count => count+1)
+          }
+          if(playerr.index2 === 'O'){
+            SetPlayer2Score(count => count+1)
+          }
+        }
+      }
     }
-    if(playerr.index1  === playerr.index1 && playerr.index4  === playerr.index1 && playerr.index7  === playerr.index1){
-      setWinText(playerr.index1, 'Wins')
-      console.log('aaara')
+    if(playerr.index4 !== ''){
+      if(playerr.index4  === playerr.index4 && playerr.index5  === playerr.index4 && playerr.index6  === playerr.index4){
+        if(hasWon === false){
+        setWinText(playerr.index4 +'  Wins')
+        setLetter('')
+        setHasWon(true)
+        if(playerr.index4 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index4 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+        
+      }
     }
-  }
-  if(playerr.index2 !== ''){
-    if(playerr.index2  === playerr.index2 && playerr.index4  === playerr.index2 && playerr.index6  === playerr.index2){
-      setWinText(playerr.index2, 'Wins')
-      console.log('aaara')
     }
-  }
-  if(playerr.index4 !== ''){
-    if(playerr.index4  === playerr.index4 && playerr.index5  === playerr.index4 && playerr.index6  === playerr.index4){
-      setWinText(playerr.index4, 'Wins')
-      console.log('aaara')
-    }
-  }
-  if(playerr.index3 !== ''){
-    if(playerr.index3  === playerr.index3 && playerr.index5  === playerr.index3 && playerr.index7 === playerr.index3){
-      setWinText(playerr.index3, 'Wins')
-    }
-    if(playerr.index3  === playerr.index3 && playerr.index6  === playerr.index3 && playerr.index9  === playerr.index3){
-      setWinText(playerr.index3, 'Wins')
-      console.log('ara')
-    }
+    if(playerr.index3 !== ''){
 
-  } if(playerr.index7 !== ''){
-    if(playerr.index7  === playerr.index7 && playerr.index8  === playerr.index7 && playerr.index9  === playerr.index7){
-      setWinText(playerr.index7, 'Wins')
-      console.log('aasara')
+      if(playerr.index3  === playerr.index3 && playerr.index5  === playerr.index3 && playerr.index7 === playerr.index3){
+        if(hasWon === false){
+        setWinText(playerr.index3+ '  Wins')
+        setLetter('')
+        setHasWon(true)
+        if(playerr.index3 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index3 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+      }
+      }
+      
+      if(playerr.index3  === playerr.index3 && playerr.index6  === playerr.index3 && playerr.index9  === playerr.index3){
+        if(hasWon === false){
+          setWinText(playerr.index3+ '  Wins')
+          setTogglePlayer(null)
+          console.log('Yep me')
+          setHasWon(true)
+          if(playerr.index3 === 'X'){
+            SetPlayer1Score(count => count+1)
+          }
+          if(playerr.index3 === 'O'){
+            SetPlayer2Score(count => count+1)
+          }
+      }
+      setLetter('')
+      }
     }
-  }
+    if(playerr.index7 !== ''){
+      if(playerr.index7  === playerr.index7 && playerr.index8  === playerr.index7 && playerr.index9  === playerr.index7){
+        if(hasWon === false){
+        setWinText(playerr.index7+ '  Wins')
+        setLetter('')
+        setHasWon(true)
+        if(playerr.index7 === 'X'){
+          SetPlayer1Score(count => count+1)
+        }
+        if(playerr.index7 === 'O'){
+          SetPlayer2Score(count => count+1)
+        }
+        
+        }
+      }
+    }
   }
   const UpdateState = (name) => {
-    handleClick();
+   setWinText('')
     if(name==='index1'){
-      setPlayer( prevState => ({...prevState, index1:letter}));
+      if(playerr.index1 === ''){
+        if(hasWon === false){
+          console.log('yeah')
+          handleClick();
+          setPlayer( prevState => ({...prevState, index1:letter}));
+        }
+      }
+    
     }
     if(name==='index2'){
-      setPlayer( prevState => ({...prevState, index2:letter}));
+      
+      if(playerr.index2 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index2:letter}));
+        } 
+      }
+      
     }
     if(name==='index3'){
-      setPlayer( prevState => ({...prevState, index3:letter}));
+      if(playerr.index3 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index3:letter}));
+        }
+      }
+      
     }
     if(name==='index4'){
-      setPlayer( prevState => ({...prevState, index4:letter}));
+      if(playerr.index4 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index4:letter}));
+        }
+      }
+      
     }
     if(name==='index5'){
-      setPlayer( prevState => ({...prevState, index5:letter}));
+      if(playerr.index5 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index5:letter}));
+        }
+      }
+     
     }
     if(name==='index6'){
-      setPlayer( prevState => ({...prevState, index6:letter}));
+      if(playerr.index6 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index6:letter}));
+        }
+      }
+      
     }
     if(name==='index7'){
-      setPlayer( prevState => ({...prevState, index7:letter}));
+      if(playerr.index7 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index7:letter}));
+        }
+      }
+     
     }
     if(name==='index8'){
-      setPlayer( prevState => ({...prevState, index8:letter}));
+      if(playerr.index8 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index8:letter}));
+        }
+      }
+      
     }
     if(name==='index9'){
-      setPlayer( prevState => ({...prevState, index9:letter}));
+      if(playerr.index9 === ''){
+        if(hasWon === false){
+          handleClick();
+          setPlayer( prevState => ({...prevState, index9:letter}));
+        }
+      }
+
     }
+  }
+  const findDraw= () =>{
+    if( hasWon === false && playerr.index1 !== '' && playerr.index2 !== '' && playerr.index3 !== '' && playerr.index4 !== '' && playerr.index5 !== '' && playerr.index6 !== '' && playerr.index7 !== '' && playerr.index8 !== '' && playerr.index9 !== '' ){
+      setWinText('It is a shameful draw')
+
+    }
+  }
+
+  const restartGame =() =>{
+    setWinText('Next Round')
+    setHasWon(false)
+    setLetter('X')
+    setTogglePlayer(true)
+    setPlayer({
+      index1:'',index2:'',index3:'' ,
+      index4:'',index5:'',index6:'',
+      index7:'',index8:'',index9:''
+    })
   }
   return (
     <>
-    <button >Start GAme</button>
-    <h1>{wintext} </h1>
+  
+    <div className="scorecounter">
+      <div className={`scores ${toggleplayer ?  'underline' : ''}`}>
+          <h2 className="bold">X</h2>
+          <p>{player1Score}</p>
+          
+      </div>
+      <div className={`scores ${!toggleplayer ?  'underline' : ''}`}>
+          <h2 className="bold">O</h2>
+          <p>{player2Score}</p>
+
+      </div>
+      </div>    
+
+    <h1>{wintext}</h1>
       <div className="grid">
-       
         <div onClick={()=>{UpdateState('index1');}} className="item corners"><p>{playerr.index1}</p></div>
         <div onClick={()=>{UpdateState('index2');}} className="item"><p>{playerr.index2}</p></div>
         <div onClick={()=>{UpdateState('index3');}} className="item"><p>{playerr.index3}</p></div>
@@ -115,9 +295,9 @@ function App() {
         <div onClick={()=>{UpdateState('index8');}} className="item bottom" ><p>{playerr.index8}</p></div>
         <div onClick={()=>{UpdateState('index9');}} className="item bottom"><p>{playerr.index9}</p></div>
        </div>
-       <button >Restart game</button>
+       <button className="restart" onClick={restartGame}>Restart game</button>
        <p></p>
-    </>
+    </ >
   )
 }
 
